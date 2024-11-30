a7fcb8c8b992da2492888f5f7161b88b4f0c55a9
ivanleoncastle <australarg@hotmail.com>
2024-11-30 14:19:15 -0300

    punto 8
    terminando punto 9

---
+++ retaurante.py
@@ -0,0 +1,14 @@
+class Restaurante:
+    def __init__(self, restaurante_nombre, tipo_comida):
+        
+        self.restaurante_nombre = restaurante_nombre
+        self.tipo_comida = tipo_comida
+
+    def describir_restaurante(self):
+       
+        print(f"Restaurante: {self.restaurante_nombre}")
+        print(f"Tipo de comida: {self.tipo_comida}")
+
+    def abrir_restaurante(self):
+       
+        print(f"¡El restaurante {self.restaurante_nombre} ahora está abierto!")
